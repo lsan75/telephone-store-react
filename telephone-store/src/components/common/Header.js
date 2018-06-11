6e1755a617289506cd035778c168d13ed184c407
stephane hordoir <stephane.hordoir@gmail.com>
2018-06-11 07:37:40 +0200

    ajout des Link

--- telephone-store/src/components/common/Header.js
+++ telephone-store/src/components/common/Header.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 class Header extends React.Component {
   render() {
-    return <div>This is the header</div>;
+    return (
+      <div>
+        This is the header
+        <NavLink to="/phones" activeClassName="active">
+          List of Phones
+        </NavLink>
+        <NavLink to="/" activeClassName="active">
+          Home
+        </NavLink>
+      </div>
+    );
   }
 }
 
